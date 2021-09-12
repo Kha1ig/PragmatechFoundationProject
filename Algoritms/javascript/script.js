856c314857009639572755be3134c9146f514051
Xaliq Rehimov <rehimov.xaliq@internet.ru>
2021-09-12 18:18:10 +0400

    Presentation

---
+++ Algoritms/javascript/script.js
@@ -0,0 +1,31 @@
+/*obj={
+    first:function(){
+        console.log("first")
+    },
+    second:function(){
+        console.log("second")
+    },
+    third:function(){
+        console.log("third");
+    }
+}
+
+obj.first().second().third()
+
+obj.second().first()*/
+
+/*
+function qrup(ad, soyad, yas) {
+    this.ad=ad;
+    this.soyad=soyad;
+    this.yas=yas;
+}
+qrup.prototype.born="born";
+let qrups = new qrup( "xaliq", "rehimov", 22, "born");
+console.log(qrups)
+prototype.qrups()
+*/
+function changecolor(haqqi) {
+    
+    haqqi.style.display='none';
+}
--- works/javascript/app.js
+++ works/javascript/app.js
@@ -1,18 +1,43 @@
 
-let a = Foo (3,4,12,45,67,48)
-function Foo(){
+let ededler=[12,45,23,67,89,1,17,90]
 
-    return(3+4+12+45+67+78)
+for(let i=0;i<8;i++){
+    if(ededler[i]%3==0){
+       console.log(`${ededler[i]} eded 3 bolunnur`);
+       
+    }
 }
-    
-    console.log(a);
-    document.write(a) 
-
-let i=0
-while(i<3){
-    
-    i++
-    console.log(i);
+for(let i=0;i<8;i++){
+    if(ededler[i]%5!==0){
+        console.log(`${ededler[i]} ədəd 5 bölünmür`)
+    }
 }
 
+
+let reqemler = [12,45,23,67,89,1,17,90]
+
+for( let i=0;i<reqemler.length;i++){
+strX=reqemler[i].toString()
+
+console.log(`${strX} `);
+
+}
+//let ededler = "12,45,23,67,89,1,17,90"
+
+//const Arr = ededler.split("");
+
+//console.log(Arr);
+//console.log(Arr [0+1])
+//Number(Arr[0] + 2)
+//console.log(Arr)
+
+//for(let i=0;i>0;i++){
+   // if(Arr[0]+Arr[1]>6){
+     //   console.log('${Arr} ededi ekranda goster')
+  //  }
     
+//}
+//console.log(Arr)
+
+
+
