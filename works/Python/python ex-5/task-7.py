2eaa396ce501e54c8ef3733d9502f33763099e5b
Xaliq Rehimov <rehimov.xaliq@internet.ru>
2021-10-06 18:36:31 +0400

    tapşırıqlarda dəyişikliklər etdim

--- works/Python/python ex-5/task-7.py
+++ works/Python/python ex-5/task-7.py
@@ -1,6 +1,11 @@
-numberone=int(input('birinci ededi daxil edin: '))
-numbertwo=int(input('birinci ededi daxil edin: '))
-
-multiply=numberone*numbertwo
-
-print('netice: ',multiply)
+import math
+numberone=input('birinci ededi daxil edin: ')
+A = 1
+# create a result list
+result = []
+for i in range(0, len(numberone), A):
+       
+    result.append(int(numberone[i : i + A]))
+  
+result1 = math.prod(result)
+print("The resultant list : " + str(result1))
---
+++ works/Python/python ex-5/task-9.py
@@ -0,0 +1,16 @@
+n=11
+
+if n%2==0:
+    print(n/2)
+
+else:
+    n+=1
+    print((n/2))
+    m=n/2
+    print((m/2))
+    d=m/2+1
+    print((d))
+    print((d/2))
+    b=d/2
+    print((b/2))
+    
